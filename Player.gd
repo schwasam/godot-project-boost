4c59b3158611b96818fd70e8ecde19ad408a2f9c
Samuel Schwager <samuel.schwager@gmail.com>
2023-10-18 15:36:05 +0200

    add audio and particles for success and failure

--- Player.gd
+++ Player.gd
@@ -8,24 +8,40 @@ extends RigidBody3D
 
 var is_transitioning: bool = false
 
+# Audio
+@onready var rocket_audio: AudioStreamPlayer3D = $RocketAudio
 @onready var success_audio: AudioStreamPlayer = $SuccessAudio
 @onready var explosion_audio: AudioStreamPlayer = $ExplosionAudio
-@onready var rocket_audio: AudioStreamPlayer3D = $RocketAudio
+
+# Particles
+@onready var booster_particles: GPUParticles3D = $BoosterParticles
+@onready var right_booster_particles: GPUParticles3D = $RightBoosterParticles
+@onready var left_booster_particles: GPUParticles3D = $LeftBoosterParticles
+@onready var success_particles: GPUParticles3D = $SuccessParticles
+@onready var explosion_particles: GPUParticles3D = $ExplosionParticles
 
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
+		apply_central_force(basis.y * thrust * delta)
+		booster_particles.emitting = true
 		if not rocket_audio.playing:
 			rocket_audio.play()
-		apply_central_force(basis.y * thrust * delta)
 	else:
+		booster_particles.emitting = false
 		rocket_audio.stop()
 
 	if Input.is_action_pressed("rotate_left"):
 		apply_torque(Vector3(0.0, 0.0, torque_thrust * delta))
+		right_booster_particles.emitting = true
+	else:
+		right_booster_particles.emitting = false
 
 	if Input.is_action_pressed("rotate_right"):
 		apply_torque(Vector3(0.0, 0.0, -torque_thrust * delta))
+		left_booster_particles.emitting = true
+	else:
+		left_booster_particles.emitting = false
 
 
 func _on_body_entered(body: Node) -> void:
@@ -41,9 +57,10 @@ func _on_body_entered(body: Node) -> void:
 func complete_level(next_level_file: String) -> void:
 	print("Level complete!")
 	rocket_audio.stop()
-	success_audio.play()
 	set_physics_process(false)
 	is_transitioning = true
+	success_particles.emitting = true
+	success_audio.play()
 	var tween = create_tween()
 	Assert.ignore(tween.tween_interval(success_audio.stream.get_length()))
 	Assert.ignore(tween.tween_callback(get_tree().change_scene_to_file.bind(next_level_file)))
@@ -52,9 +69,10 @@ func complete_level(next_level_file: String) -> void:
 func crash_sequence() -> void:
 	print("Kaboom!")
 	rocket_audio.stop()
-	explosion_audio.play()
 	set_physics_process(false)
 	is_transitioning = true
+	explosion_particles.emitting = true
+	explosion_audio.play()
 	var tween = create_tween()
 	Assert.ignore(tween.tween_interval(explosion_audio.stream.get_length()))
 	Assert.ignore(tween.tween_callback(get_tree().reload_current_scene))
